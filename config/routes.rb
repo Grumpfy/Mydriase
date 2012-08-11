6a9672ee89291b436dc02e56d35988cb862f98ce
Stephane Guy <stephane.guy@allegorithmic.com>
2012-08-12 00:14:58 +0200

    changed site index

--- config/routes.rb
+++ config/routes.rb
@@ -58,6 +58,8 @@ Mydriase::Application.routes.draw do
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
+  root :to => 'gestion#index', as: 'gestion' 
+
   # See how all your routes lay out with "rake routes"
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
