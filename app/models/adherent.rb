1ebae3de5f4bab68dc259e297f84e0fc9b5f347f
Stephane Guy <stephane.guy@allegorithmic.com>
2012-08-13 16:34:33 +0200

    updated models relations

--- app/models/adherent.rb
+++ app/models/adherent.rb
@@ -1,3 +1,4 @@
 class Adherent < ActiveRecord::Base
   attr_accessible :adresse, :code_postal, :mail, :nom, :portable, :prenom, :remarques, :tel, :ville
+  has_many :inscriptions, dependent: :destroy
 end
--- app/models/inscription.rb
+++ app/models/inscription.rb
@@ -1,3 +1,6 @@
 class Inscription < ActiveRecord::Base
   attr_accessible :adherent_id, :attelier_id, :remarques, :stage_id
+  belongs_to :stage
+  belongs_to :atelier
+  belongs_to :adherent
 end
