882f9ed4bcd3a86007e9e20f806bf11e32e2bacb
Stephane Guy <stephane.guy@allegorithmic.com>
2012-08-14 18:32:18 +0200

    added inscription validation

--- app/models/inscription.rb
+++ app/models/inscription.rb
@@ -1,7 +1,16 @@
 class Inscription < ActiveRecord::Base
   attr_accessible :adherent_id, :atelier_id, :remarques, :stage_id
   validates :stage_id, :atelier_id, :adherent_id, presence: true
+  validate :stage_and_atelier_match
   belongs_to :stage
   belongs_to :atelier
   belongs_to :adherent
+
+  private
+
+  def stage_and_atelier_match
+    if stage_id != atelier.stage_id
+      errors.add(:atelier_id, "attelier didn't belongs to the selected stage")
+    end
+  end
 end
