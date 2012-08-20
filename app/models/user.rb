4c9bc6aeef7413bd7780c6816f52da42227cc78b
Stephane Guy <stephane.guy@allegorithmic.com>
2012-08-21 01:32:52 +0200

    tweak users scaffold for password stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :admin, :name, :password_digest
+  attr_accessible :admin, :name, :password, :password_confirmation
+  validates :name, presence: true, uniqueness: true 
+  has_secure_password
 end
