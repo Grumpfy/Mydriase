30e437e284ea58c4eb11156c24dc612dd22cc57b
Stephane Guy <stephane.guy@allegorithmic.com>
2012-09-08 13:19:35 +0200

    forbid last administrator deletion

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,14 @@ class User < ActiveRecord::Base
   attr_accessible :admin, :name, :password, :password_confirmation
   validates :name, presence: true, uniqueness: true 
   has_secure_password
+  after_destroy :ensure_an_admin_remains
+
+  private
+
+  def ensure_an_admin_remains
+    if User.where(admin: true).count().zero?
+      raise "Impossible de supprimer le dernier administrateur"
+    end 
+  end
+
 end
