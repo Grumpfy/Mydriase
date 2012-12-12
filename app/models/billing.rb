1edc1b24a2a92a31f2506cad927d243630b74995
Stephane Guy <stephane.guy@allegorithmic.com>
2012-12-13 00:29:05 +0100

    update billings and operations validation

--- app/models/billing.rb
+++ app/models/billing.rb
@@ -1,7 +1,7 @@
 class Billing < ActiveRecord::Base
   attr_accessible :inscription_id, :description, :price
   validates :description, :price, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :price, numericality: {greater_than_or_equal_to: 0.00}
   belongs_to :inscription
   validates_associated :inscription
 
--- app/models/operation.rb
+++ app/models/operation.rb
@@ -1,7 +1,7 @@
 class Operation < ActiveRecord::Base
   attr_accessible :inscription_id, :description, :method, :price
   validates :description, :method, :price, presence: true
-  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  validates :price, numericality: {greater_than_or_equal_to: 0.00}
   belongs_to :inscription
   validates_associated :inscription
 
